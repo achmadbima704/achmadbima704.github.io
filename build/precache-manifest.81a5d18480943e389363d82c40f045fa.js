39c5f6bb185a524bd25c889e7861ad19385e231c
achmadbima704 <achmadbima704@gmail.com>
2019-04-29 01:07:39 +0700

    first init

---
+++ build/precache-manifest.81a5d18480943e389363d82c40f045fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add3ff565c6bca7be3276568824a5e5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da3ff978c1319cff1010",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "cb52c3eb9f810ab3bf11",
+    "url": "/static/js/2.0f453d96.chunk.js"
+  },
+  {
+    "revision": "da3ff978c1319cff1010",
+    "url": "/static/js/main.59790a8a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is TodoList app v1.0.0. It is part of React crash course</p>
+    </React.Fragment>
+  )
+}
